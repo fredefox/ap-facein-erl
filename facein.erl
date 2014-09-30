fb2114068151fbf305caaaa639f7bcb051cd8fd6
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2014-09-30 17:23:44 +0200

    Add skeleton

---
+++ facein.erl
@@ -0,0 +1,27 @@
+-module(facein.erl).
+%-export(your stuff here).
+
+"""
+ a) `start/1`
+"""
+start(N) -> throw(unimplemented).
+
+"""
+ b) `add_friend/2`
+"""
+add_friend(P, F) -> throw(unimplemented).
+
+"""
+ c) `friends/1`
+"""
+friends(P) -> throw(unimplemented).
+
+"""
+ d) `broadcast/3`
+"""
+broadcast(P, M, R) -> throw(unimplemented).
+
+"""
+ e) `received_messages/1`
+"""
+received_messages(P) -> throw(unimplemented).
