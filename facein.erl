727b1d94a3ba4c76d8e9d905af92315a955d8858
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2014-09-30 22:47:00 +0200

    Fix multiline comment mixup

--- facein.erl
+++ facein.erl
@@ -1,27 +1,45 @@
--module(facein.erl).
+-module(facein).
+-export([start/1, add_friend/2, friends/1, broadcast/3, received_messages/1]).
 %-export(your stuff here).
 
-"""
- a) `start/1`
-"""
-start(N) -> throw(unimplemented).
+%
+% a) `start/1`
+%
+start(Id) -> {
+		ok,
+		spawn(fun() -> serve(Id, []) end)
+	   }.
 
-"""
- b) `add_friend/2`
-"""
-add_friend(P, F) -> throw(unimplemented).
+serve(Id, Friends) ->
+	% Server-implementation of all responses.
+	receive
+		% TODO
+		{Pid, get_friends} -> Pid ! Friends
+	end.
 
-"""
- c) `friends/1`
-"""
+request(Pid, Request) ->
+	Pid ! {self(), Request},
+	receive
+		{Pid, Response} -> Response
+	end.
+%
+% b) `add_friend/2`
+%
+add_friend(P, F) ->
+	% Somehow add the result of the following call to `P`.
+	P = request(F, get_friends) ++ P.
+
+%
+% c) `friends/1`
+%
 friends(P) -> throw(unimplemented).
 
-"""
- d) `broadcast/3`
-"""
+%
+% d) `broadcast/3`
+%
 broadcast(P, M, R) -> throw(unimplemented).
 
-"""
- e) `received_messages/1`
-"""
+%
+% e) `received_messages/1`
+%
 received_messages(P) -> throw(unimplemented).
