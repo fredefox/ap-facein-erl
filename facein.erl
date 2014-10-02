c2d0cae1c0a03750d36bc8fc1595ef48a1910c90
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2014-10-02 17:13:04 +0200

    Add some definitions after help from Simon.
    This commit gives implementations for:
    
        * `add_friend`
        * `get_friends`
        * `broadcast`
    
    At least `braodcast` is incomplete

--- facein.erl
+++ facein.erl
@@ -5,41 +5,68 @@
 %
 % a) `start/1`
 %
-start(Id) -> {
-		ok,
-		spawn(fun() -> serve(Id, []) end)
-	   }.
+start(Id) ->
+		Pid = spawn(fun() -> serve(Id, gb_sets:empty()) end),
+		{ok, Pid}.
 
 serve(Id, Friends) ->
 	% Server-implementation of all responses.
 	receive
 		% TODO
-		{Pid, get_friends} -> Pid ! Friends
+		{Pid, get_friends} ->
+			Pid ! {Pid, Friends},
+			serve(Id, Friends);
+		{Pid, {add_friend, F}} ->
+			Name = request(F, get_name),
+			% TODO: runtime error occurs here if `Friends` contain F
+			F = gb_sets:enter(F, Name, Friends),
+			Pid ! {Pid, ok},
+			serve(Id, F);
+		{Pid, get_name} ->
+			Pid ! {Pid, Id};
+		{Pid, {broadcast, M, R}} ->
+			% TODO: This function should not wait for the other to return (which it
+			% does currently). I.e.: Use `Pid`.
+			if R == 0 -> [];
+			   R > 0 ->
+				   % loop through friends and send message
+				   %
+				   % TODO: We might have actually already broadcast this message.
+				   % Consider the sample graph in the case where Susan has
+				   % broadcast some message with radius >3. She will then later
+				   % receive a message from Andrzej and Jen with the same
+				   % message and the radius 2 less. She should ideally only
+				   % forward one of these messages.
+				   Broadcast = fun(Friend) -> Friend ! {broadcast, M, R-1} end,
+				   lists:map(Broadcast, gb_trees:keys(Friends))
+			end;
+		{Pid, get_messages} -> throw(undefined)
 	end.
 
+% Synchronous requests
 request(Pid, Request) ->
 	Pid ! {self(), Request},
 	receive
 		{Pid, Response} -> Response
 	end.
+
 %
 % b) `add_friend/2`
 %
 add_friend(P, F) ->
-	% Somehow add the result of the following call to `P`.
-	P = request(F, get_friends) ++ P.
+	request(P, {add_friend, F}).
 
 %
 % c) `friends/1`
 %
-friends(P) -> throw(unimplemented).
+friends(P) -> request(P, get_friends).
 
 %
 % d) `broadcast/3`
 %
-broadcast(P, M, R) -> throw(unimplemented).
+broadcast(P, M, R) -> request(P, {broadcast, M, R}).
 
 %
 % e) `received_messages/1`
 %
-received_messages(P) -> throw(unimplemented).
+received_messages(P) -> request(P, get_messages).
