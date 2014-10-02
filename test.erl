0eed09498dc5b604f440ac0d5d91cd9228099f81
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2014-10-02 21:44:48 +0200

    Add test-case for add_friend

--- test.erl
+++ test.erl
@@ -10,3 +10,11 @@ test00() ->
 test01() ->
 	{ok, Pid} = facein:start(42),
 	facein:friends(Pid).
+
+% Test `add_friend`
+test02() ->
+	{ok, P} = facein:start(42),
+	{ok, Q} = facein:start(1337),
+	facein:add_friend(P, Q),
+	% P should now be friends with Q aka 1337.
+	facein:friends(P).
