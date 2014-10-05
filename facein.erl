a9f783a7235e0e048cb22c40b230b3fffe4cf2b5
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2014-10-05 15:18:12 +0200

    Refactor `TODO`-notes

--- facein.erl
+++ facein.erl
@@ -11,10 +11,9 @@ start(Id) ->
 serve(Id, Friends, Msgs) ->
 	% Server-implementation of all responses.
 	receive
-		% TODO
 		{Pid, get_friends} ->
-			% This does not follow specification. It does indeed return a list
-			% of two-tuples, but the tuples are reversed. They should be:
+			% TODO This does not follow specification. It does indeed return a
+			% list of two-tuples, but the tuples are reversed. They should be:
 			%
 			%     {Id, Pid}
 			%
@@ -25,7 +24,6 @@ serve(Id, Friends, Msgs) ->
 			serve(Id, Friends, Msgs);
 		{Pid, {add_friend, F}} ->
 			Name = request(F, get_name),
-			% TODO: runtime error occurs here if `Friends` contain F
 			NewFriends = gb_trees:enter(F, Name, Friends),
 			Pid ! {self(), ok},
 			serve(Id, NewFriends, Msgs);
@@ -48,15 +46,6 @@ serve(Id, Friends, Msgs) ->
 					   true ->
 						   serve(Id, Friends, NewMsgs);
 					   _ ->
-						   % loop through friends and send message
-						   %
-						   % TODO: We might have actually already broadcast
-						   % this message. Consider the sample graph in the
-						   % case where Susan has broadcast some message with
-						   % radius >3. She will then later receive a message
-						   % from Andrzej and Jen with the same message and the
-						   % radius 2 less. She should ideally only forward one
-						   % of these messages.
 						   Broadcast =
 						       fun(Friend) -> Friend ! {broadcast, M, R-1} end,
 						   lists:map(Broadcast, gb_trees:keys(Friends)),
